5cebac7a238a6835c52c4ba94bf7c0e9a8f6da38
fadellasrg <fadellasrg@gmail.com>
2021-02-24 19:34:04 +0700

    insert history

--- src/model/history.js
+++ src/model/history.js
@@ -5,7 +5,9 @@ module.exports = {
         return new Promise ((resolve, reject) => {
             connection.query(`SELECT history.from_id as from_id, history.to_id AS to_id, users.image as image, users.name as name, history.amount, status, notes, UNIX_TIMESTAMP(created_at) AS time FROM history 
             LEFT JOIN users ON history.to_id = users.id
-            WHERE history.from_id = ${id} AND ${searchParams} LIKE '%${search}%' ORDER BY ${param} ${sort}
+            WHERE history.from_id = ${id} OR 
+            history.to_id = ${id} AND
+            ${searchParams} LIKE '%${search}%' ORDER BY ${param} ${sort}
             LIMIT ${offset}, ${limit}
             `,(err, result) => {
                 if(err){
@@ -30,4 +32,17 @@ module.exports = {
             })
         })
     },
+    mInsertHistory: (data) => {
+        return new Promise ((resolve, reject)=>{
+            connection.query(`INSERT INTO history (from_id, to_id, amount, status, notes)
+            VALUES ( '${data.from_id}', '${data.to_id}', '${data.amount}', '${data.status}', '${data.notes}' ) `
+            , (err, result)=>{
+                if(err){
+                    reject(new Error(err))
+                }else{
+                    resolve(result)
+                }
+            })
+        })
+    }
 }
--- src/route/history.js
+++ src/route/history.js
@@ -2,11 +2,13 @@ const express = require('express')
 const Router = express.Router()
 
 const { 
-    listHistory
+    listHistory,
+    insertHistory
 } = require('../controller/history')
 const { authentication } = require('../helper/middleware/auth')
 
 Router
     .get('/api/history/:id', authentication, listHistory)
+    .post('/api/history', authentication, insertHistory)
 
 module.exports= Router
