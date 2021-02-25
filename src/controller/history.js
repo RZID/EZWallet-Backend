6342f6c49ec805bd57f1c6fbae0c99bc595ca763
fadellasrg <fadellasrg@gmail.com>
2021-02-25 13:49:18 +0700

    update history

--- src/controller/history.js
+++ src/controller/history.js
@@ -13,26 +13,26 @@ const {
 const { success, failed, notFound } = require('../helper/response')
 
 module.exports = {
-    listHistory: async (req, res)=>{
+    listHistory: (req, res)=>{
         try {
             const id = req.params.id
-            const searchParams = req.query.searchParams ? req.query.searchParams : 'name'
+            // const searchParams = req.query.searchParams ? req.query.searchParams : 'user_from.name'
             const search = req.query.search ? req.query.search : ''
             const param = req.query.param ? req.query.param : 'created_at'
             const sort = req.query.sort ? req.query.sort : 'ASC'
             const limit = req.query.limit ? req.query.limit : 6
             const page = req.query.page ? req.query.page : 1
             const offset = page===1 ? 0 : (page-1)*limit
-            const responseTotal = await mTotal(id, searchParams, search) // count total page
+            // const responseTotal = await mTotal(id, search) // count total page
             
-            mListHistory(id, searchParams, search, param, sort, offset, limit)
+            mListHistory(id, search, param, sort, offset, limit)
             .then((response)=>{
                 const data = response
                 const pagination = {
                     page: page,
                     limit: limit,
-                    totalData: responseTotal[0].total,
-                    totalPage: Math.ceil(responseTotal[0].total / limit)
+                    // totalData: responseTotal[0].total,
+                    // totalPage: Math.ceil(responseTotal[0].total / limit)
                 }
                 if(response.length > 0){
                     success(res, data, pagination, 'Get all history success')
