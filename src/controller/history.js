1e91f4651ddffb9c20a187d024d57c11713daeb9
fadellasrg <fadellasrg@gmail.com>
2021-02-25 12:07:02 +0700

    update transfer

--- src/controller/history.js
+++ src/controller/history.js
@@ -6,6 +6,10 @@ const {
     mTransferSuccess
 } = require('../model/history')
 
+const {
+    mDetailUser
+} = require('../model/users')
+
 const { success, failed, notFound } = require('../helper/response')
 
 module.exports = {
@@ -82,18 +86,24 @@ module.exports = {
             failed(res, 'Internal server error', [])
         }
     },
-    transferSuccess: (req, res) =>{
+    transferSuccess: async (req, res) =>{
         try {
             const data = req.body
+            const detail = await mDetailUser(data.from_id)
+
             if(!data.from_id || !data.to_id || !data.amount){
-                failed(res, 'All textfield is required!', [])
-            }else{
+                    failed(res, 'All textfield is required!', [])
+            }
+
+            if(detail[0].balance > data.amount){
                 mTransferSuccess(data)
                 .then((response)=>{
                     success(res, response, {}, 'Transfer success')
                 }).catch((err)=>{
                     failed(res, 'All textfield is required!', [])
                 })
+            }else{
+                failed(res, 'Check your balance', [])
             }
         } catch (error) {
             failed(res, 'Internal server error', [])
