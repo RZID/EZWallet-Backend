60523f71683e6177097bc5beca6321cb6e8b4314
RZIDInc <rdhanukpb@gmail.com>
2021-02-27 08:28:15 +0700

    Local RZIDInc

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,12 +1,12 @@
-const bcrypt = require ('bcrypt')
-const { 
+const bcrypt = require('bcrypt')
+const {
     mRegister,
     mCheckEmail,
     mUpdateUser,
     mDetailUser,
     mAllUser,
     mTotal
-} = require ('../model/users')
+} = require('../model/users')
 const { success, failed, notFound } = require('../helper/response')
 const jwt = require('jsonwebtoken')
 const { JWT_SECRET } = require('../helper/env')
@@ -15,17 +15,17 @@ const fs = require('fs')
 module.exports = {
     login: (req, res) => {
         const body = req.body
-        mCheckEmail(body.email).then( async (response) => {
-            if(response.length === 1){
+        mCheckEmail(body.email).then(async (response) => {
+            if (response.length === 1) {
                 const checkPassword = await bcrypt.compare(body.password, response[0].password)
-                if(checkPassword && response[0].pin.length != 0){
-                    
+                if (checkPassword && response[0].pin.length != 0) {
+
                     const dataUser = {  // data to be encrypted by JWT
                         id: response[0].id,
                         email: response[0].email
                     }
                     const token = jwt.sign(dataUser, JWT_SECRET)
-                    
+
                     res.json({
                         message: 'Login success',
                         token,     // same property and value
@@ -33,14 +33,14 @@ module.exports = {
                         id: response[0].id,
                         pin: true
                     })
-                }else if(checkPassword && response[0].pin.length === 0){
-                    
+                } else if (checkPassword && response[0].pin.length === 0) {
+
                     const dataUser = {  // data to be encrypted by JWT
                         id: response[0].id,
                         email: response[0].email
                     }
                     const token = jwt.sign(dataUser, JWT_SECRET)
-                    
+
                     res.json({
                         message: 'Login success',
                         token,     // same property and value
@@ -48,21 +48,21 @@ module.exports = {
                         id: response[0].id,
                         pin: false
                     })
-                }else{
+                } else {
                     failed(res, 'Login failed, please check your password', {})
                 }
-            }else{
-                notFound(res,"Email not found", {})
+            } else {
+                notFound(res, "Email not found", {})
             }
         }).catch((err) => {
-            if(!body.email || !body.password){
+            if (!body.email || !body.password) {
                 failed(res, 'Please input all field', err)
-            }else{
+            } else {
                 failed(res, 'Internal server error', err)
             }
         })
     },
-    register: async(req, res) => {
+    register: async (req, res) => {
         const body = req.body
         const data = {
             name: body.name,
@@ -72,10 +72,10 @@ module.exports = {
             pin: '',
             phone: '+62'
         }
-        mCheckEmail(data.email).then( async (response) => {
-            if(response.length >= 1){
+        mCheckEmail(data.email).then(async (response) => {
+            if (response.length >= 1) {
                 failed(res, 'Email has been registered', {})
-            }else{
+            } else {
                 // use salt to add a unique code in password
                 const salt = await bcrypt.genSalt(10) // 10 to make code more unique (optional)
                 const password = await bcrypt.hash(body.password, salt)
@@ -87,9 +87,9 @@ module.exports = {
                     pin: data.pin,
                     phone: data.phone,
                 }
-                if( !user.name || !user.email || !user.password ){
+                if (!user.name || !user.email || !user.password) {
                     failed(res, 'All textfield is required!', [])
-                }else{
+                } else {
                     mRegister(user).then((response) => {
                         success(res, {}, {}, 'Register success')
                     }).catch((err) => {
@@ -107,37 +107,37 @@ module.exports = {
             const id = req.params.id
             const detail = await mDetailUser(id)
 
-            if(data.pin){
+            if (data.pin) {
                 const salt = await bcrypt.genSalt(10)
                 data.pin = await bcrypt.hash(data.pin, salt)
             }
-            if(req.file){
-                if(detail[0].image === 'default.png'){
+            if (req.file) {
+                if (detail[0].image === 'default.png') {
                     data.image = req.file.filename
                     mUpdateUser(data, id)
-                    .then((response)=>{
-                        success(res, response, {}, 'Update profile success')
-                    }).catch((err)=>{
-                        failed(res, 'Internal server error', [])
-                    }) 
-                }else{
+                        .then((response) => {
+                            success(res, response, {}, 'Update profile success')
+                        }).catch((err) => {
+                            failed(res, 'Internal server error', [])
+                        })
+                } else {
                     data.image = req.file.filename
                     const path = `./public/images/${detail[0].image}`
                     fs.unlinkSync(path)
                     mUpdateUser(data, id)
-                    .then((response)=>{
-                        success(res, response, {}, 'Update profile success')
-                    }).catch((err)=>{
-                        failed(res, 'Internal server error', [])
-                    }) 
+                        .then((response) => {
+                            success(res, response, {}, 'Update profile success')
+                        }).catch((err) => {
+                            failed(res, 'Internal server error', [])
+                        })
                 }
-            }else{
+            } else {
                 mUpdateUser(data, id)
-                .then((response)=>{
-                    success(res, response, {}, 'Update success')
-                }).catch((err)=>{
-                    failed(res, 'Internal server error', [])
-                })
+                    .then((response) => {
+                        success(res, response, {}, 'Update success')
+                    }).catch((err) => {
+                        failed(res, 'Internal server error', [])
+                    })
             }
         } catch (error) {
             failed(res, 'Internal server error', [])
@@ -146,19 +146,19 @@ module.exports = {
     loginPIN: (req, res) => {
         const body = req.body
         const id = req.params.id
-        mDetailUser(id).then( async (response) => {
-            if(response.length === 1){
+        mDetailUser(id).then(async (response) => {
+            if (response.length === 1) {
                 const checkPIN = await bcrypt.compare(body.pin, response[0].pin)
-                if(checkPIN){
+                if (checkPIN) {
                     success(res, {}, {}, 'Login success')
-                }else{
+                } else {
                     failed(res, 'Login failed, please check your PIN number', {})
                 }
             }
         }).catch((err) => {
-            if(!body.pin){
+            if (!body.pin) {
                 failed(res, 'Please input all field', err)
-            }else{
+            } else {
                 failed(res, 'Internal server error', err)
             }
         })
@@ -166,26 +166,26 @@ module.exports = {
     detailUser: (req, res) => {
         const id = req.params.id
         mDetailUser(id)
-        .then((response) => {
-            if(response.length>0) {
-                const data = {
-                    id: response[0].id,
-                    name: response[0].name,
-                    email: response[0].email,
-                    image: response[0].image,
-                    phone: response[0].phone,
-                    balance: response[0].balance
+            .then((response) => {
+                if (response.length > 0) {
+                    const data = {
+                        id: response[0].id,
+                        name: response[0].name,
+                        email: response[0].email,
+                        image: response[0].image,
+                        phone: response[0].phone,
+                        balance: response[0].balance
+                    }
+                    success(res, data, {}, 'Get detail user')
+                } else {
+                    notFound(res, "Id user not found", {})
                 }
-                success(res, data, {}, 'Get detail user')
-            } else {
-                notFound(res,"Id user not found", {})
-            }
-        })
-        .catch((err) => {
-            failed(res, 'Internal server error', err)
-        })
+            })
+            .catch((err) => {
+                failed(res, 'Internal server error', err)
+            })
     },
-    listUser: async (req, res)=>{
+    listUser: async (req, res) => {
         try {
             const id = req.params.id
             const searchParams = req.query.searchParams ? req.query.searchParams : 'name'
@@ -194,26 +194,26 @@ module.exports = {
             const sort = req.query.sort ? req.query.sort : 'ASC'
             const limit = req.query.limit ? req.query.limit : 6
             const page = req.query.page ? req.query.page : 1
-            const offset = page===1 ? 0 : (page-1)*limit
+            const offset = page === 1 ? 0 : (page - 1) * limit
             const responseTotal = await mTotal(searchParams, search) // count total page
-        
+
             mAllUser(id, searchParams, search, param, sort, offset, limit)
-            .then((response)=>{
-                const data = response
-                const pagination = {
-                    page: page,
-                    limit: limit,
-                    totalData: responseTotal[0].total,
-                    totalPage: Math.ceil(responseTotal[0].total / limit)
-                }
-                if(response.length > 0){
-                    success(res, data, pagination, 'Get all users success')
-                }else{
-                    notFound(res,"Data unavailable", data)
-                }
-            }).catch((err)=>{
-                failed(res, 'Internal server error', err)
-            })
+                .then((response) => {
+                    const data = response
+                    const pagination = {
+                        page: page,
+                        limit: limit,
+                        totalData: responseTotal[0].total,
+                        totalPage: Math.ceil(responseTotal[0].total / limit)
+                    }
+                    if (response.length > 0) {
+                        success(res, data, pagination, 'Get all users success')
+                    } else {
+                        notFound(res, "Data unavailable", data)
+                    }
+                }).catch((err) => {
+                    failed(res, 'Internal server error', err)
+                })
         } catch (error) {
             failed(res, 'Internal server error', [])
         }
